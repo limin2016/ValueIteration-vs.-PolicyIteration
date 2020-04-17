8063e3060741460f21e0ad8a1b7079a992d2436d
nimiL <1466299085@qq.com>
2020-04-16 22:59:09 -0400

    delete unused attributes and functions

--- State.py
+++ State.py
@@ -6,21 +6,11 @@ class State(object):
         self.row_index = row_index
         self.col_index = col_index
         self.noises = [0,0,0,0]
-        self.policy_change = True
-        self.optimal_policy = False
-        self.has_value = False
         self.dir = '-'
 
     def set_initial_policy(self, noises):
-        #self.noises = copy.deepcopy(noises)
         self.noises[0] = noises[0]
         self.noises[1] = noises[1]
         self.noises[2] = noises[3]
         self.noises[3] = noises[2]
 
-    def update_policy(self, direction, noises):
-        self.policy_dir = direction
-        self.noises[direction] = noises[0]
-        self.noises[(direction + 2) % 4] = noises[3]
-        self.noises[(direction + 1) % 4] = noises[1]
-        self.noises[(direction + 3) % 4] = noises[2]
